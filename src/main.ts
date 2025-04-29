24753022240c3e3f554b85351e7c7f1db7360598
Kayxue <s0990006@gmail.com>
2025-04-29 23:58:35 +0800

    Init to new repo

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,11 @@
-//@ts-types="npm:@types/express-session"
-import session from "npm:express-session";
-//@ts-types="npm:@types/passport"
-import passport from "npm:passport";
-import nhttp from "jsr:@nhttp/nhttp";
-import cors from "jsr:@nhttp/nhttp/cors";
-import memoryStore from "npm:memorystore";
+import session from "express-session";
+import passport from "passport";
+import nhttp from "@nhttp/nhttp";
+import cors from "@nhttp/nhttp/cors";
+import memoryStore from "memorystore";
 import { initStrategy } from "./strategies/local.ts";
 import { authenticated } from "./middleware.ts";
-//@ts-types="npm:@types/cookie-signature"
-import signature from "npm:cookie-signature";
+import signature from "cookie-signature";
 
 const secret = "akpEUnT8iZIFjm-CwmwIf";
 
@@ -21,42 +18,42 @@ app.use(cors({ credentials: true }));
 const MemoryStore = memoryStore(session);
 
 app.use(
-	session({
-		cookie: { maxAge: 60000 * 60 * 24, secure: true },
-		store: new MemoryStore({
-			checkPeriod: 60000 * 60 * 24,
-		}),
-		resave: true,
-		saveUninitialized: false,
-		secret,
-	}),
+  session({
+    cookie: { maxAge: 60000 * 60 * 24, secure: true },
+    store: new MemoryStore({
+      checkPeriod: 60000 * 60 * 24,
+    }),
+    resave: true,
+    saveUninitialized: false,
+    secret,
+  }),
 );
 
 app.use(passport.initialize());
 app.use(passport.session());
 
 app.get("/", () => {
-	return "Hello World!";
+  return "Hello World!";
 });
 
 app.post(
-	"/login",
-	passport.authenticate("local"),
-	({ response, user, sessionID }) => {
-		response.cookie("connect.sid", `s:${signature.sign(sessionID, secret)}`);
-		return user;
-	},
+  "/login",
+  passport.authenticate("local"),
+  ({ response, user, sessionID }) => {
+    response.cookie("connect.sid", `s:${signature.sign(sessionID, secret)}`);
+    return user;
+  },
 );
 
 app.get("/logout", ({ session }) => {
-	session.destroy();
-	return "Logged out";
+  session.destroy();
+  return "Logged out";
 });
 
 app.get("/protected", authenticated, () => {
-	return "Protected";
+  return "Protected";
 });
 
 app.listen(3000, () => {
-	console.log("Server is ready");
+  console.log("Server is ready");
 });

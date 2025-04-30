e22464979bcb0b1ed519af1f35aa60ffc6f00aee
Kayxue <s0990006@gmail.com>
2025-04-30 10:39:08 +0800

    Add role to session user

--- src/strategies/local.ts
+++ src/strategies/local.ts
@@ -2,18 +2,20 @@ import passport from "passport";
 import { Strategy as LocalStrategy } from "passport-local";
 
 export function initStrategy() {
-  passport.serializeUser((user: { username: string }, done) => {
-    done(null, user.username);
+  passport.serializeUser((user: { username: string; role: string }, done) => {
+    done(null, { username: user.username, role: user.role });
   });
 
   passport.deserializeUser((payload, done) => {
+    // TODO: Fetch user from database using id and role
+    console.log(payload);
     done(null, payload);
   });
 
   passport.use(
     new LocalStrategy((username, password, done) => {
       if (username === "admin" && password === "admin") {
-        return done(null, { username });
+        return done(null, { username, role: "admin" });
       }
       return done(null, false);
     }),

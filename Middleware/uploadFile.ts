7149d141230915bb011958f4e32a636ec589e178
Yew02 <zongyung6656@gmail.com>
2025-05-12 17:26:15 +0800

    add multipart as middleware

---
+++ Middleware/uploadFile.ts
@@ -0,0 +1,16 @@
+import { multipart } from "jsr:@nhttp/nhttp";
+
+export const uploadDocument = multipart.upload({
+  name: "verficationDocument",
+  dest: "./uploads/verficationDocument",
+  maxSize: "2mb",
+  maxCount: 2,
+  accept: ["pdf", "jpg", "jpeg", "png"],
+  required: true,
+  callback: (file) => {
+    const timestamp = Date.now();
+    const randomSuffix = Math.random().toString(36).substring(2, 8);
+    const extension = file.name.split(".").pop();
+    file.filename = `${timestamp}_${randomSuffix}.${extension}`;
+  }
+});
